8dc48a07cd4213035d70db3734a60d184f1c48fe
frankmata17 <frank.mata17@gmail.com>
2025-03-15 10:21:58 -0600

    port change

--- w02/cse341-ww-student-code-w02-2-team-start/swagger.js
+++ w02/cse341-ww-student-code-w02-2-team-start/swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'Temple API',
     description: 'API documentation for temple routes',
   },
-  host: 'localhost:3000',
+  host: 'localhost:8080',
   schemes: ['http'],
 };
 
